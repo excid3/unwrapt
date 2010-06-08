eb7f4c0f2e00735bc2033055a3c154998bae4612
Chris Oliver <excid3@gmail.com>
2010-06-08 01:23:44 -0500

    Licensed

--- src/Download.py
+++ src/Download.py
@@ -1,14 +1,33 @@
+#    Unwrapt - cross-platform package system emulator
+#    Copyright (C) 2010 Chris Oliver <chris@excid3.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import logging
 import os
 import urllib
 import sys
 
 
+#TODO: Integrate urlgrabber RateEstimator
+
+
 def text_hook(url, filename, existing, total):
-    #print "%s: %f" % (filename, existing/float(total)*100)
     frac = float(existing)/float(total)
     bar = '='*int(25*frac)
-    sys.stdout.write("\r%-25.25s %3i%% |%-25.25s| 5sB 8s ETA " % \
+    sys.stdout.write("\r%-25.25s %3i%% |%-25.25s|" % \ # 5sB 8s ETA " % \
         (filename, frac*100, bar))
     if frac == 1:
         sys.stdout.write("\n")
--- src/definitions/aptdef/__init__.py
+++ src/definitions/aptdef/__init__.py
@@ -1,3 +1,20 @@
+#    Unwrapt - cross-platform package system emulator
+#    Copyright (C) 2010 Chris Oliver <chris@excid3.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import logging
 
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -122,9 +139,7 @@ class Apt(DefinitionBase):
         for repo in self.__iter_repositories():
             main_url = url_join(repo.to_url(), url_join(self.architecture, "Packages"))
             
-            
-
-        
+                    
 #    def on_get_available_binary_packages(self):
 #        pass
         

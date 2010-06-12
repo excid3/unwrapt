b97324a9ae80b6f1cf1fa22283f81a83d7b07da9
Chris Oliver <excid3@gmail.com>
2010-06-12 01:05:06 -0500

    functional urllib downloader implementation

--- src/Download.py
+++ src/Download.py
@@ -16,59 +16,240 @@
 
 
 import logging
+import math
 import os
+import time
 import urllib
 import sys
 
 
-#TODO: Integrate urlgrabber RateEstimator
-
-
-def text_hook(url, filename, existing, total):
-    frac = float(existing)/float(total)
-    bar = '='*int(25*frac)
-    sys.stdout.write("\r%-25.25s %3i%% |%-25.25s|" % \ # 5sB 8s ETA " % \
-        (filename, frac*100, bar))
-    if frac == 1:
-        sys.stdout.write("\n")
-
-
-def download(url, filename=None, reporthook=text_hook):
-    if not filename:
-        filename = url.strip("/").split("/")[-1]
-
-    urlopener = urllib.FancyURLopener()
+class Downloader:
+    def __init__(self, url, filename=None, reporthook=None):
+        
+        # Create a filename if we need to
+        if not filename:
+            filename = url.strip("/").split("/")[-1]
+        
+        if not reporthook:
+            reporthook = self.textreport
+        
+        self.url = url
+        self.filename = filename
+        self.hook = reporthook
+        self.existing = self.__check_partial()
+        self.re = RateEstimator()
+        
+        
+    def start(self):
     
-    exist = 0
-    if os.path.exists(filename):
-        dest = open(filename, "ab")
-        exist = os.path.getsize(filename)
-        urlopener.addheader("Range", "bytes=%s-" % exist)
+        # Configure the 
+        urlopener = urllib.FancyURLopener()
+        urlopener.addheader("Range", "bytes=%s-" % self.existing)
+        
+        page = urlopener.open(self.url)
+        
+        total = int(page.headers["Content-Length"])
+        total += self.existing        
+        
+        self.re.start(total)
+        
+        dest = open(self.filename, "ab")
+        
+        bytes = 0
+        while self.existing <= total:
+            data = page.read(8192)
+            bytes += len(data)
+            
+            self.re.update(bytes)
+            self.hook(self.existing + bytes, total)
+            
+            if not data:
+                break
+        
+            dest.write(data)
+            
+            
+        page.close()
+        dest.close()
+        
+    def textreport(self, existing, total):
+    
+        frac = float(existing)/float(total)
+        bar = '=' * int(25*frac) 
+        
+        sys.stdout.write("\r%-25.25s %3i%% |%-25.25s|" % \
+            (self.filename,
+             frac*100,
+             bar))
+        if frac == 1:
+            sys.stdout.write("\n")
+    
+    
+    def __check_partial(self):
+        if os.path.exists(self.filename):
+            return os.path.getsize(self.filename)
+        else:
+            return 0
+            
+            
+class RateEstimator:
+    def __init__(self, timescale=5.0):
+        self.timescale = timescale
+
+    def start(self, total=None, now=None):
+        if now is None: now = time.time()
+        self.total = total
+        self.start_time = now
+        self.last_update_time = now
+        self.last_amount_read = 0
+        self.ave_rate = None
+        
+    def update(self, amount_read, now=None):
+        if now is None: now = time.time()
+        if amount_read == 0:
+            # if we just started this file, all bets are off
+            self.last_update_time = now
+            self.last_amount_read = 0
+            self.ave_rate = None
+            return
+
+        #print 'times', now, self.last_update_time
+        time_diff = now         - self.last_update_time
+        read_diff = amount_read - self.last_amount_read
+        self.last_update_time = now
+        self.last_amount_read = amount_read
+        self.ave_rate = self._temporal_rolling_ave(\
+            time_diff, read_diff, self.ave_rate, self.timescale)
+        #print 'results', time_diff, read_diff, self.ave_rate
+        
+    #####################################################################
+    # result methods
+    def average_rate(self):
+        "get the average transfer rate (in bytes/second)"
+        return self.ave_rate
+
+    def elapsed_time(self):
+        "the time between the start of the transfer and the most recent update"
+        return self.last_update_time - self.start_time
+
+    def remaining_time(self):
+        "estimated time remaining"
+        if not self.ave_rate or not self.total: return None
+        return (self.total - self.last_amount_read) / self.ave_rate
+
+    def fraction_read(self):
+        """the fraction of the data that has been read
+        (can be None for unknown transfer size)"""
+        if self.total is None: return None
+        elif self.total == 0: return 1.0
+        else: return float(self.last_amount_read)/self.total
+
+    #########################################################################
+    # support methods
+    def _temporal_rolling_ave(self, time_diff, read_diff, last_ave, timescale):
+        """a temporal rolling average performs smooth averaging even when
+        updates come at irregular intervals.  This is performed by scaling
+        the "epsilon" according to the time since the last update.
+        Specifically, epsilon = time_diff / timescale
+
+        As a general rule, the average will take on a completely new value
+        after 'timescale' seconds."""
+        epsilon = time_diff / timescale
+        if epsilon > 1: epsilon = 1.0
+        return self._rolling_ave(time_diff, read_diff, last_ave, epsilon)
+    
+    def _rolling_ave(self, time_diff, read_diff, last_ave, epsilon):
+        """perform a "rolling average" iteration
+        a rolling average "folds" new data into an existing average with
+        some weight, epsilon.  epsilon must be between 0.0 and 1.0 (inclusive)
+        a value of 0.0 means only the old value (initial value) counts,
+        and a value of 1.0 means only the newest value is considered."""
+        
+        try:
+            recent_rate = read_diff / time_diff
+        except ZeroDivisionError:
+            recent_rate = None
+        if last_ave is None: return recent_rate
+        elif recent_rate is None: return last_ave
+
+        # at this point, both last_ave and recent_rate are numbers
+        return epsilon * recent_rate  +  (1 - epsilon) * last_ave
+
+    def _round_remaining_time(self, rt, start_time=15.0):
+        """round the remaining time, depending on its size
+        If rt is between n*start_time and (n+1)*start_time round downward
+        to the nearest multiple of n (for any counting number n).
+        If rt < start_time, round down to the nearest 1.
+        For example (for start_time = 15.0):
+         2.7  -> 2.0
+         25.2 -> 25.0
+         26.4 -> 26.0
+         35.3 -> 34.0
+         63.6 -> 60.0
+        """
+
+        if rt < 0: return 0.0
+        shift = int(math.log(rt/start_time)/math.log(2))
+        rt = int(rt)
+        if shift <= 0: return rt
+        return float(int(rt) >> shift << shift)
+        
+        
+def format_time(seconds, use_hours=0):
+    if seconds is None or seconds < 0:
+        if use_hours: return '--:--:--'
+        else:         return '--:--'
     else:
-        dest = open(filename, "wb")
-        
-    page = urlopener.open(url)
-    total = int(page.headers["Content-Length"])
-    finished = exist >= total
-    logging.debug(exist, total, finished)
-    total = total + exist
-        
-    bytes = 0
-    while not finished:
-        data = page.read(8192)
-        
-        reporthook(url, filename, exist + bytes, total)
-        
-        if not data:
-            break    
-    
-        dest.write(data)
-        bytes += len(data)
-        
-    page.close()
-    dest.close()
-    
-if __name__ == "__main__":
-    download("http://launchpad.net/keryx/stable/0.92/+download/keryx_0.92.4.zip")
-    print "voila!"
+        seconds = int(seconds)
+        minutes = seconds / 60
+        seconds = seconds % 60
+        if use_hours:
+            hours = minutes / 60
+            minutes = minutes % 60
+            return '%02i:%02i:%02i' % (hours, minutes, seconds)
+        else:
+            return '%02i:%02i' % (minutes, seconds)
 
+
+def format_number(number, SI=0, space=' '):
+    """Turn numbers into human-readable metric-like numbers"""
+    symbols = ['',  # (none)
+               'k', # kilo
+               'M', # mega
+               'G', # giga
+               'T', # tera
+               'P', # peta
+               'E', # exa
+               'Z', # zetta
+               'Y'] # yotta
+    
+    if SI: step = 1000.0
+    else: step = 1024.0
+
+    thresh = 999
+    depth = 0
+    max_depth = len(symbols) - 1
+    
+    # we want numbers between 0 and thresh, but don't exceed the length
+    # of our list.  In that event, the formatting will be screwed up,
+    # but it'll still show the right number.
+    while number > thresh and depth < max_depth:
+        depth  = depth + 1
+        number = number / step
+
+    if type(number) == type(1) or type(number) == type(1L):
+        # it's an int or a long, which means it didn't get divided,
+        # which means it's already short enough
+        format = '%i%s%s'
+    elif number < 9.95:
+        # must use 9.95 for proper sizing.  For example, 9.99 will be
+        # rounded to 10.0 with the .1f format string (which is too long)
+        format = '%.1f%s%s'
+    else:
+        format = '%.0f%s%s'
+        
+    return(format % (float(number or 0), space, symbols[depth]))
+    
+
+d = Downloader("http://launchpad.net/keryx/stable/0.92/+download/keryx_0.92.4.zip")
+d.start()
